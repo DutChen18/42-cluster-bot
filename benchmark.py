8127a458c19217ccaa055ea9839092ea8cf82ab8
Ruben van Meerten <ruben.vanmeerten@hotmail.nl>
2022-03-15 16:27:17 +0100

    benchmark switches player 1 and player 2 after every run

--- benchmark.py
+++ benchmark.py
@@ -6,15 +6,24 @@ inp = sys.argv
 player1_wins = 0
 player2_wins = 0
 total_time = 0
+turn = 0
 for i in range(0, int(inp[4])):
 	start = time.time()
-	program = subprocess.run([inp[1], inp[2], inp[3]], capture_output=True, text=True)
-	if program.stdout == "Player 1 wins\n":
-		player1_wins += 1
-	elif program.stdout == "Player 2 wins\n":
-		player2_wins += 1
+	if turn % 2 == 0:
+		program = subprocess.run([inp[1], inp[2], inp[3]], capture_output=True, text=True)
+		if program.stdout == "Player 1 wins\n":
+			player1_wins += 1
+		elif program.stdout == "Player 2 wins\n":
+			player2_wins += 1
+	else:
+		program = subprocess.run([inp[1], inp[3], inp[2]], capture_output=True, text=True)
+		if program.stdout == "Player 1 wins\n":
+			player2_wins += 1
+		elif program.stdout == "Player 2 wins\n":
+			player1_wins += 1
 	end = time.time()
 	total_time += end - start
+	turn += 1
 total_time /= int(inp[4])
 print("player1 wins: " + str(player1_wins) + " player2 wins: " + str(player2_wins))
 print("avarage time per run: " + str(total_time) + "\n")
