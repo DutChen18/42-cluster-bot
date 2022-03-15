9de2f59831f54e1133c6888fb5812928812115a9
Ruben van Meerten <ruben.vanmeerten@hotmail.nl>
2022-03-15 16:08:41 +0100

    added benchmarking

---
+++ benchmark.py
@@ -0,0 +1,20 @@
+import sys
+import time
+import subprocess
+
+inp = sys.argv
+player1_wins = 0
+player2_wins = 0
+total_time = 0
+for i in range(0, int(inp[4])):
+	start = time.time()
+	program = subprocess.run([inp[1], inp[2], inp[3]], capture_output=True, text=True)
+	if program.stdout == "Player 1 wins\n":
+		player1_wins += 1
+	elif program.stdout == "Player 2 wins\n":
+		player2_wins += 1
+	end = time.time()
+	total_time += end - start
+total_time /= int(inp[4])
+print("player1 wins: " + str(player1_wins) + " player2 wins: " + str(player2_wins))
+print("avarage time per run: " + str(total_time) + "\n")
