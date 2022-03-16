ddc5a3e034b15a4cb80147b4c18cd86d4a84c52f
Ruben van Meerten <ruben.vanmeerten@hotmail.nl>
2022-03-16 16:41:32 +0100

    more info
    !

--- benchmark.py
+++ benchmark.py
@@ -6,12 +6,7 @@ import threading
 import argparse
 
 inp = sys.argv
-player1_wins = 0
-player2_wins = 0
-games_ran = 0
-total_time = 0
-player1_games_played = 0
-player2_games_played = 0
+player1_wins, player2_wins, games_ran, total_time, player1_games_played, player2_games_played, player1_out_of_chips, player2_out_of_chips, player1_timed_out, player2_timed_out = 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0
 
 parser = argparse.ArgumentParser(description='Benchmark 2 bots versus each other\n')
 parser.add_argument('game_path', type=str, help='Path of game')
@@ -21,14 +16,21 @@ parser.add_argument('--games', type=int, help='Amount of games to run', default=
 parser.add_argument('--threads', type=int, help='Amount of threads to run', default=1)
 args = parser.parse_args()
 
-
 def run_battle():
-	global args, player1_wins, player2_wins, total_time, games_ran, player1_games_played, player2_games_played
+	global args, player1_wins, player2_wins, total_time, games_ran, player1_games_played, player2_games_played, player1_out_of_chips, player2_out_of_chips, player1_timed_out, player2_timed_out
 	turn = 0
 	for x in range(0, int(args.games / args.threads)):
 		start = time.time()
 		if turn % 2 == 0:
 			program = subprocess.run([args.game_path, args.player_1_path, args.player_2_path], capture_output=True, text=True)
+			if program.stderr == "Player 1 is out of chips\n":
+				player1_out_of_chips += 1
+			elif program.stderr == "Player 2 is out of chips\n":
+				player2_out_of_chips += 1
+			elif program.stderr == "Player 1 timed out\n":
+				player1_timed_out += 1
+			elif program.stderr == "Player 2 timed out\n":
+				player2_timed_out += 1
 			if program.stdout == "Player 1 wins\n":
 				player1_wins += 1
 			elif program.stdout == "Player 2 wins\n":
@@ -36,6 +38,14 @@ def run_battle():
 			player1_games_played += 1
 		else:
 			program = subprocess.run([args.game_path, args.player_2_path, args.player_1_path], capture_output=True, text=True)
+			if program.stderr == "Player 1 is out of chips\n":
+				player2_out_of_chips += 1
+			elif program.stderr == "Player 2 is out of chips\n":
+				player1_out_of_chips += 1
+			elif program.stderr == "Player 1 timed out\n":
+				player2_timed_out += 1
+			elif program.stderr == "Player 2 timed out\n":
+				player1_timed_out += 1
 			if program.stdout == "Player 1 wins\n":
 				player2_wins += 1
 			elif program.stdout == "Player 2 wins\n":
@@ -53,8 +63,11 @@ if __name__ == "__main__":
 		threads[x].start()
 	for x in range(len(threads)):
 		threads[x].join()
-	print('Ran '+ str(games_ran) + ' games on ' + str(args.threads) + ' threads\n')
-	print("Player_1 wins: " + str(player1_wins) + "  - Player_2 wins: " + str(player2_wins))
-	print("Player_1 games played first: " + str(player1_games_played) + " - Player_2 games played first: " + str(player2_games_played))
+	print('\nRan '+ str(games_ran) + ' games on ' + str(args.threads) + ' thread(s)\n')
+	print("Player 1 wins: " + str(player1_wins) + " - Player 2 wins: " + str(player2_wins) + "\n")
+	print("Games played:	Player 1: " + str(player1_games_played) + " - Player 2: " + str(player2_games_played))
+	print("Out of chips:	Player 1: " + str(player1_out_of_chips) + " - Player 2: " + str(player2_out_of_chips))
+	print("Timed out:	Player 1: " + str(player1_timed_out) + " - Player 2: " + str(player2_timed_out))
+	print("Total time: " + str(total_time))
 	total_time /= games_ran
-	print("Avarage time per run: " + str(total_time) + "\n")
+	print("Avarage time per run: " + str(total_time))
